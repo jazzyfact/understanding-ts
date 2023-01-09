0970a03625635976694012699e7f973a026d5f99
jazzyfact <jazzyfact2000@gmail.com>
2023-01-10 00:10:11 +0900

    init, 중첩된 개체 및 타입

---
+++ app.js
@@ -0,0 +1,6 @@
+var person = {
+    // const person = {
+    name: "mimi",
+    age: 30
+};
+console.log(person.name);
---
+++ app.ts
@@ -0,0 +1,10 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+const person = {
+  name: "mimi",
+  age: 30,
+};
+
+console.log(person.name);
