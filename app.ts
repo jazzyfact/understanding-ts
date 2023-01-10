422c3215c7dc355e18022b24ea08a0c6cb2d776c
jazzyfact <jazzyfact2000@gmail.com>
2023-01-10 23:25:49 +0900

    배열 타입

--- app.ts
+++ app.ts
@@ -5,6 +5,14 @@
 const person = {
   name: "mimi",
   age: 30,
+  hobbies: ["Sports", "Cooking"],
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
